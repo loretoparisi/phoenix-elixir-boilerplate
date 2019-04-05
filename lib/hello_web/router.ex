1b409f8b3626a4c25db2f8ab26e9ac5afc4891c4
Loreto Parisi <loretoparisi@gmail.com>
2019-04-05 21:15:56 +0200

    Initial Import

---
+++ lib/hello_web/router.ex
@@ -0,0 +1,28 @@
+defmodule HelloWeb.Router do
+  use HelloWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", HelloWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", HelloWeb do
+  #   pipe_through :api
+  # end
+end
